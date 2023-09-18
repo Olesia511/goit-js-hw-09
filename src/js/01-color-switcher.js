b94754b1df5871bbb1a4e0daeb150f8a75861561
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-09-18 23:51:10 +0300

    hw-9/1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBgColor = document.querySelector('body');
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStart);
+
+function onStart() {
+  const id = setInterval(() => {
+    stopBtn.addEventListener('click', onStop);
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    const colorHex = getRandomHexColor();
+    bodyBgColor.style.backgroundColor = `${colorHex}`;
+
+    function onStop() {
+      clearInterval(id);
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+    }
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
